790ee6d3e2235e6826bb3b363b7f4e308c030002
Алеська <1912977@mail.ru>
2022-10-06 20:53:48 +0300

    admin_site

--- website/main/models.py
+++ website/main/models.py
@@ -1,19 +1,23 @@
-# from django.db import models
-#
-#
-# class Task(models.Model):
-#     title = models.CharField('Название', max_length=50)
-#     task = models.TextField('Описание')
-#
-#     def __str__(self):
-#         return self.title
-#
-#
-# class Main(models.Model):
-#     title = models.CharField('Заголовок', max_length=55)
-#     main = models.TextField('Основное')
-#     img = models.FileField(upload_to='img/')
-#     data = models.DateTimeField(auto_now_add=True)
-#
-#     def __str__(self):
-#         return self.main, self.img
+from django.db import models
+
+
+class Task(models.Model):
+    title = models.CharField('Название', max_length=50)
+    task = models.TextField('Описание')
+
+    def __str__(self):
+        return self.title
+    #
+    # class Meta:
+    #     verbose_name='Задача'
+    #     verbose_name_plural='Задачи'
+
+
+class Main(models.Model):
+    title = models.CharField('Заголовок', max_length=55)
+    main = models.TextField('Основное')
+    img = models.FileField(upload_to='img/')
+    data = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- website/main/views.py
+++ website/main/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task, Main
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    tasks = Task.objects.all()
+    mains = Main.objects.all()
+    context = {'title': 'Главная страница', 'tasks': tasks, 'mains': mains}
+    return render(request, 'main/index.html', context)
 
 
 def about(request):
